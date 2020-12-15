2731dbff13a2b5a68758e58873cba717b70fbff6
Igor Smolkov <smolkov.igor@gmail.com>
2020-12-15 20:07:58 +0700

    input-type-range customization

--- src/components/event-slider/event-slider.js
+++ src/components/event-slider/event-slider.js
@@ -1,5 +1,24 @@
 import './event-slider.scss'
 
+const props = {
+    eventsTotal: 2
+}
+
 export default function() {
-    alert('event-slider.js')
+    const eventSliderElement = document.querySelector('.event-slider')
+    const rangeElement = eventSliderElement.querySelector('.event-slider__range')
+    const valueElement = eventSliderElement.querySelector('.event-slider__value')
+
+    rangeElement.min = 0;
+    rangeElement.max = props.eventsTotal;
+    rangeElement.step = 0.1;
+    rangeElement.value = props.eventsTotal;
+
+    valueElement.innerText = rangeElement.value
+
+    rangeElement.addEventListener('change', ()=>{
+        const currentValue = Math.round(rangeElement.value)
+        rangeElement.value = currentValue
+        valueElement.innerText = currentValue
+    })
 }
