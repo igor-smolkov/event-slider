7616010604b2e9a958b060217b841486cc95b7d4
Igor Smolkov <smolkov.igor@gmail.com>
2021-01-15 16:04:59 +0700

    refact, data, pointer, hover, focus, keyboard controll

--- src/components/event-slider/event-slider.js
+++ src/components/event-slider/event-slider.js
@@ -2,28 +2,29 @@ import './event-slider.scss'
 
 export default class EventSlider {
     constructor(props) {
-        this.props = props; //убрать
-
         this.elementSlider = document.querySelector('.event-slider');
         this.elementRange = this.elementSlider.querySelector('.event-slider__range');
         this.elementEvents = this.elementSlider.querySelector('.events');
 
-        this.currentValue = props.length-1;
+        this.length = props.length;
+        this.currentValue = this.length-1;
         this.isSlideStart = false;
 
-        this.initRange(props.length);
+        this.initRange(this.length);
 
-        this.elementSlider.addEventListener('mousedown', this.mouseDown.bind(this));
-        this.elementSlider.addEventListener('mouseup', this.mouseUp.bind(this));
-        this.elementSlider.addEventListener('mousemove', this.mouseMove.bind(this));
+        this.elementSlider.addEventListener('pointerdown', (e)=>this.pointerDown(e));
+        this.elementSlider.addEventListener('pointerup', (e)=>this.pointerUp(e));
+        this.elementSlider.addEventListener('pointermove', this.pointerMove.bind(this), true);
+        this.elementSlider.addEventListener('keydown', (e)=>this.keydown(e));
 
         props.forEach((event, index) => {
             const eventElement = document.createElement('option');
             eventElement.value = index;
             eventElement.setAttribute('label', event.date);
+            eventElement.setAttribute('data-name', event.name);
             this.elementEvents.append(eventElement);
-            eventElement.style.left = (this.elementSlider.offsetWidth/props.length)*index+
-                ((this.elementSlider.offsetWidth/props.length)-eventElement.offsetWidth)/2 + 'px';
+            eventElement.style.left = (this.elementSlider.offsetWidth/this.length)*index+
+                ((this.elementSlider.offsetWidth/this.length)-eventElement.offsetWidth)/2 + 'px';
         })
 
         this.showValue(this.currentValue);
@@ -36,17 +37,42 @@ export default class EventSlider {
         this.elementRange.style.marginLeft = `${(this.elementSlider.offsetWidth/length)/2-9}px`;
         this.elementRange.style.width = `${(this.elementSlider.offsetWidth-this.elementSlider.offsetWidth/length)+20}px`;
     }
-    mouseDown() {
+    pointerDown(e) {
         this.isSlideStart = true;
+        if (e.pointerType === 'mouse') {
+            if (!e.target.classList.contains('event-slider__range')) {
+                this.elementRange.classList.add('hover');
+            }
+        } else if (e.target.classList.contains('event-slider__range')) {
+            this.elementRange.classList.add('hover');
+            this.elementRange.focus();
+        }
     }
-    mouseUp() {
+    pointerUp(e) {
         this.isSlideStart = false;
-        this.elementRange.value = this.currentValue;
+        this.doMove(this.currentValue);
+        this.elementRange.classList.remove('hover');
+        if (e.pointerType === 'mouse') { this.elementRange.focus(); }
     }
-    mouseMove() {
-        if (!this.isSlideStart) return false;
+    pointerMove() {
+        if (!this.isSlideStart) return;
         this.currentValue = Math.round(this.elementRange.value);
         this.showValue(this.currentValue);
+    }
+    keydown(e) {
+        if (document.activeElement.classList.contains('event-slider__range')) {
+            if (e.code === 'ArrowLeft') {
+                this.currentValue = this.currentValue - 1 < 0 ? 0 : this.currentValue - 1;
+            }
+            if (e.code === 'ArrowRight') {
+                this.currentValue = this.currentValue + 1 > this.length-1 ? this.length-1 : this.currentValue + 1;
+            }
+            this.doMove(this.currentValue);
+            this.showValue(this.currentValue);
+        }
+    }
+    doMove() {
+        this.elementRange.value = this.currentValue;
     }
     showValue(value){
         const currentEventScaleElement = this.elementEvents.querySelector(`option[value='${value}']`);
@@ -56,7 +82,7 @@ export default class EventSlider {
 
         const currentEventInfoElement = document.createElement('p');
         currentEventInfoElement.classList.add('events__current');
-        currentEventInfoElement.innerText = this.props[value].name;
+        currentEventInfoElement.innerText = currentEventScaleElement.dataset.name;
         this.elementEvents.append(currentEventInfoElement);
         
         let calcLeft = (currentEventScaleElement.offsetWidth/2 + currentEventScaleElement.offsetLeft) - currentEventInfoElement.offsetWidth/2;
